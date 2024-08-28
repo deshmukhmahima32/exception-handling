d160a1ec8372e4e12a01b7ac8eda5305e47b6c9d
deshmukhmahima32 <deshmukhmahima40@gmail.com>
2024-08-28 15:06:55 +0530

    Add files via upload

---
+++ exception21.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+class Mismat{
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+       try{
+         System.out.println("Enter 1st value");
+         int a = sc.nextInt();
+
+         System.out.println("Enter 2nd value");
+         int b = sc.nextInt();
+
+         int c = a / b;
+         System.out.println("Result : "+c);
+       }
+       catch(ArithmeticException e){
+          System.out.println("Denominator can not be zero");
+       }
+       catch(InputMismatchException e){
+         e.printStackTrace();
+        //  System.out.println("Please enter integer only");
+       }
+       System.out.println("At the end...");
+    }
+} 
+    
+
